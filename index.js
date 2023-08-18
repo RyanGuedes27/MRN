b5f600fd055f6a258bb142a1850b3af913b42d47
RyanGuedes27 <ryanguedes21@outlook.com>
2023-08-17 21:29:42 -0300

    config route

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+const userRoute = require("../MRN/src/routes/user.route");
+const port = 3000;
 
-app.listen(3000);
+app.use(express.json());
+app.use("/user", userRoute);
+
+app.listen(3000, () => console.log(`servidor rodando na porta ${port}`));
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,20 @@
+const create = (req, res) => {
+  const { name, username, email, password, avatar, background } = req.body;
+
+  if (!name || !username || !email || !password || !avatar || !background) {
+    res.status(400).send({ message: "Falta informação" });
+  }
+
+  res.status(201).send({
+    message: "User created sucessfully",
+    user: {
+      name,
+      username,
+      email,
+      avatar,
+      background,
+    },
+  });
+};
+
+module.exports = { create };
